5d9f91c06f797de222640cd1e6287333340da857
yosef-chaim <yoske208@gmail.com>
2024-12-17 17:49:12 +0200

    aaa

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("hhhnhhhih");
+import express, { Express } from "express";
+import "dotenv/config";
+
+const app: Express = express();
+
+app.use(express.json());
+
+console.log("aaaaa");
+
+app.listen(process.env.PORT || 4050, () => {
+  console.log(
+    ` listen to port http://localhost: ${process.env.PORT || 4000} }  `
+  );
+});
